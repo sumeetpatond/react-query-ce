4b743859d458cfdbc2bdbb812a215dd546cd606e
Sumeet Patond <sumeetpatond@Sumeets-MacBook-Air.local>
2023-05-09 02:10:51 +0530

    fetching without react query

--- src/components/Heros.page.jsx
+++ src/components/Heros.page.jsx
@@ -1,5 +1,30 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 function Heros() {
-  return <div>Heros</div>;
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:4000/superheroes").then((res) => {
+      setData(res.data);
+      setIsLoading(false);
+    });
+  }, []);
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+
+  return (
+    <>
+      <h2>Super Heroes Page</h2>
+      {data.map((hero) => {
+        // eslint-disable-next-line react/jsx-key
+        return <div>{hero.name}</div>;
+      })}
+    </>
+  );
 }
 
 export default Heros;
