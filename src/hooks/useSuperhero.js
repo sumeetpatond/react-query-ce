01014556aee4d3446f02e9521afdcd367ee96cef
Sumeet Patond <sumeetpatond@druva.com>
2023-08-27 18:14:59 +0530

    initial data

--- src/hooks/useSuperhero.js
+++ src/hooks/useSuperhero.js
@@ -1,12 +1,24 @@
 import axios from "axios";
-import { useQuery } from "react-query";
+import { useQuery, useQueryClient } from "react-query";
 
 const fetchSuperhero = ({ queryKey }) => {
   const heroId = queryKey[1];
   return axios.get(`http://localhost:4000/superheroes/${heroId}`);
 };
 const useSuperhero = (heroId) => {
-  return useQuery(["superheroe", heroId], fetchSuperhero);
+  const queryClient = useQueryClient();
+  return useQuery(["superheroe", heroId], fetchSuperhero, {
+    initialData: () => {
+      const hero = queryClient
+        .getQueriesData("superheroes")[0][1]
+        ?.data?.find((hero) => (hero.id = parseInt(heroId)));
+      if (hero) {
+        return { data: hero };
+      } else {
+        return undefined
+      }
+    },
+  });
 };
 
 export default useSuperhero;
